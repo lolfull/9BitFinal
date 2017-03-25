27eb47a773e4ada7be38ad320741eb2ceaffa7d6
Jian Ping Xu <jxu454@uwo.ca>
2017-03-25 02:20:02 -0400

    Add files via upload

--- 9BitTestRepo/grails-app/init/facespace/BootStrap.groovy
+++ 9BitTestRepo/grails-app/init/facespace/BootStrap.groovy
@@ -11,7 +11,11 @@ class BootStrap {
         def me = new User(username: 'me', password: 'bob', enabled: true, accountExpired: false, accountLocked: false, passwordExpired: false).save(failonError: true)
 
         UserRole.create(me, userRole)
-
+        def trans = null
+        trans = new Transaction(username: 'bob', amount: "5000", category: "housing", date: "1/1/1")
+        assert trans.save()
+        trans = new Transaction(username: 'bob', amount: "10000", category: "housing", date: "1/1/1")
+        assert trans.save()
      }
     def destroy = {
     }
